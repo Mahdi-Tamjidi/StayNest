c8f86c22eec4c36535a5d7e3d299638d06891c2d
Mahdi <mahditamjidi4@gmail.com>
2025-07-30 11:38:41 +0330

    Added Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,21 @@
+import BookingForm from "./BookingForm";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-center bg-cover h-screen'>
+      <p className="bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20">
+        The Ultimate Hotel Experience
+      </p>
+      <h1 className=" font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">
+        Discover Your Perfect Gateway Destination
+      </h1>
+      <p className="max-w-130 mt-2 text-sm md:text-base">
+        Unparalleled luxury and comfort await at the world's most exclusive
+        hotels and resorts. Start your journey today.
+      </p>
+      <BookingForm />
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 bg-indigo-500 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${
+      className={`fixed top-0 left-0  w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${
         isScrolled
           ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4"
           : "py-4 md:py-6"
@@ -76,7 +76,7 @@ const Navbar = () => {
         />
         <button
           className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${
-            isScrolled ? "text-white bg-black" : "bg-white text-black"
+            isScrolled ? "bg-white text-black" : "text-white bg-black"
           }`}
         >
           Login
